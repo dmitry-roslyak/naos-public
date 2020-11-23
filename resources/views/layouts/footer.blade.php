0a7b6454273da2686fdcbe35cac23f3942a1ce30
Dmitriy Ros <dmitri.roslyak@gmail.com>
2020-11-23 17:48:30 +0200

    footer update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,46 +1,43 @@
-<footer class="footer">
-    <div>
-        <table class="hidden-xs">
-            <tbody>
-                <tr>
-                    <td>
-                        <a href="https://laravel.com" target="_blank">
-                            <img src="/images/LaravelLogo.png">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://Vuejs.org" target="_blank">
-                            <img src="/images/Vue.png">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://webpack.js.org/" target="_blank">
-                            <img src="https://webpack.js.org/assets/icon-square-big.svg">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://sass-lang.com/" target="_blank">
-                            <img alt="Sass" src="https://sass-lang.com//assets/img/logos/logo-b6e1ef6e.svg">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://firebase.google.com/products/auth/" target="_blank">
-                            <img src="https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_96dp.png" alt="Firebase">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://getbootstrap.com/docs/3.3/" target="_blank">
-                            <img src="/images/Boostrap_logo.svg.png">
-                        </a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <h5>
+<footer>
+    <div class="container">
+        <ul class="hidden-xs">
+            <li>
+                <a href="https://laravel.com" target="_blank">
+                    <img src="/images/LaravelLogo.png" loading="lazy">
+                </a>
+            </li>
+            <li>
+                <a href="https://Vuejs.org" target="_blank">
+                    <img src="/images/Vue.png" loading="lazy">
+                </a>
+            </li>
+            <li>
+                <a href="https://webpack.js.org/" target="_blank">
+                    <img src="https://webpack.js.org/assets/icon-square-big.svg" loading="lazy">
+                </a>
+            </li>
+            <li>
+                <a href="https://getbootstrap.com/docs/3.4/" target="_blank">
+                    <img src="/images/Boostrap_logo.svg.png" loading="lazy">
+                </a>
+            </li>
+            <li>
+                <a href="https://eslint.org/" target="_blank">
+                    <img src="https://avatars3.githubusercontent.com/u/6019716?s=200&amp;v=4" alt="@eslint" loading="lazy">
+                </a>
+            </li>
+            <li>
+                <a href="https://firebase.google.com/products/auth/" target="_blank">
+                    <img src="https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_96dp.png" alt="Firebase" loading="lazy">
+                </a>
+            </li>
+        </ul>
+        <div class="contacts">
+            <a href="https://github.com/dmitry-roslyak/naos" target="_blank"><i class="fa fa-github"></i></a>
             <a href="mailto:dmitri.roslyak@gmail.com" target="_blank"><i class="fa fa-envelope"></i></a>
             <a href="https://t.me/dmitry_roslyak" target="_blank"><i class="fa fa-telegram"></i></a>
             <a href="skype:f1-forever?chat" target="_blank"><i class="fa fa-skype"></i></a>
-        </h5>
-        <h5 class="copyright">&copy;&nbsp;2017-2019&nbsp;Roslyak Dmitry</h5>
+            <h5 class="copyright">&copy;&nbsp;2017-2020&nbsp;Roslyak Dmitry</h5>
+        </div>
     </div>
 </footer>
